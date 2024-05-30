6650409bc0621cd9971c62e9468d0b843a9325d6
Peter MacAulay <144722304+PeterMacAulay7@users.noreply.github.com>
2024-05-29 21:22:39 -0300

    upload main

---
+++ barbiegame/Main.java
@@ -0,0 +1,14 @@
+import barbiegame.GameStart;
+
+/**
+ * Contains main method to execute program
+ * @author Peter MacAulay
+ * @version April 20, 2024
+ */
+public class Main {
+    public static void main(String[] args){
+        GameStart game = new GameStart();
+        game.begin();
+    }
+
+}
